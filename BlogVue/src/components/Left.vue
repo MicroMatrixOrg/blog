a9e2dc795068bb173251270b5bd8c6fbe38d352c
David <ezioaudiotrebaobao@gmail.com>
2020-10-27 21:31:30 +0800

    new matrix

---
+++ BlogVue/src/components/Left.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="left-menu">
+    左边栏目
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ BlogVue/src/components/input/Input.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="input-theme-default">
+    <form role="search" class="search-form">
+      <input
+        type="search"
+        placeholder="搜索摘要/标签/用户"
+        class="search-input"
+      />
+      <i class="fa fa-search"></i>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.search-form {
+  border: 1px solid hsla(0, 0%, 59.2%, 0.2);
+  background-color: rgba(227, 231, 236, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 2px;
+
+  > i {
+    padding: 0 0.5rem;
+    cursor: pointer;
+  }
+}
+
+.input-theme-default input[type="search"] {
+  border: none;
+  width: 12rem;
+  padding: 0.6rem 0 0.6rem 1rem;
+  box-shadow: none;
+  outline: none;
+  font-size: 1.1rem;
+  color: #666;
+  background-color: transparent;
+}
+</style>
